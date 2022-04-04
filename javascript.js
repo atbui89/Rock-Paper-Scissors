64961473d1df369a4e4f096b27793843980a8fbf
Andy Bui <at.bui89@gmail.com>
2022-04-03 20:34:20 -0400

    Added logic for computer's choice

---
+++ javascript.js
@@ -0,0 +1,7 @@
+var selections = ['Rock', 'Paper', 'Scissors'];
+var  computerChoice = selections[Math.floor(Math.random() * selections.length)]
+function computerPlay() {
+  
+}
+
+console.log(computerChoice);
